d9d160ea29c194262789af27a551d54afc6e6219
Yuki Yamaura <ymruki@gmail.com>
2023-03-18 11:07:39 +0900

    Remove extern crate test to avoid multiple definitions

--- untest_macro/src/lib.rs
+++ untest_macro/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, ItemFn};
 
 #[proc_macro_attribute]
@@ -19,24 +19,29 @@ fn generate_test(ast: &ItemFn) -> Result<TokenStream, syn::Error> {
         _ => None,
     };
 
+    let mod_name = format_ident!("untest_generated_test_{}", ident);
+
     let gen = quote! {
-        extern crate test;
+        //extern crate test;
         #[allow(non_upper_case_globals)]
         pub const #ident: test::TestDescAndFn = test::TestDescAndFn {
           desc: test::TestDesc {
-              name: test::StaticTestName(stringify!(#ident)),
-              ignore: false,
-              ignore_message: ::core::option::Option::None,
-              compile_fail: false,
-              no_run: false,
-              should_panic: test::ShouldPanic::No,
-              test_type: test::TestType::IntegrationTest,
+            name: test::StaticTestName(stringify!(#ident)),
+            ignore: false,
+            ignore_message: ::core::option::Option::None,
+            compile_fail: false,
+            no_run: false,
+            should_panic: test::ShouldPanic::No,
+            test_type: test::TestType::IntegrationTest,
           },
-          testfn: test::StaticTestFn(|| test::assert_test_result(untest_generated_test::#ident())),
-      };
-      mod untest_generated_test {
-        #gen_pub #ast
-      }
+          testfn: test::StaticTestFn(|| test::assert_test_result(
+            #mod_name::#ident()
+          )),
+        };
+        mod #mod_name {
+          use super::*;
+          #gen_pub #ast
+        }
     };
 
     Ok(gen.into())
